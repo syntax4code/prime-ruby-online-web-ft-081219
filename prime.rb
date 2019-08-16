d240a93873ba89199997b4f78ef2d1413b4242ce
Warren Loy Harper II <warrenharper2@att.net>
2019-08-16 22:17:52 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,9 @@
 def prime?(num)
-  if num < 0 or num == 0 or num == 1
+  if num < 0 or num == 0 or num == 1 #0 and 1 must be excluded they are not prime
     return false
   else
-    (2..num-1).to_a.all? do |possible_factor|
-      num % possible_factor != 0
+    (2..num-1).to_a.all? do |possible_factor| #include all other integers, put them in a array,
+      num % possible_factor != 0              #divide each num in the array
     end
   end
 end
